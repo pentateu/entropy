07b8d8f69bc7ba20fac89b1e22945fb9d712335f
Rafael Almeida <rafael@Rafaels-MacBook-Pro.local>
2018-08-30 18:33:56 +1200

    chore experimenting

--- packages/ui-common/src/web_common/core.cljs
+++ packages/ui-common/src/web_common/core.cljs
@@ -11,16 +11,15 @@
 
 (defn testInterop [] "interop text  uhuuuu !")
 
-(defn hello-world []
+(defn componentX []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h3 "I'm componentX!"]])
 
-; (reagent/render-component [hello-world]
-;                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+(defn serviceX [])
