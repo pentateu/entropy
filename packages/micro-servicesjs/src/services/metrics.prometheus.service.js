aa84663fa8aeeb393ed46acdb14a0e635c6953c4
Rafael Almeida <rafael@getproperly.com>
2018-09-02 16:14:08 +1200

    feat (metrics) prometheus setup WIP

--- packages/micro-servicesjs/src/services/metrics.prometheus.service.js
+++ packages/micro-servicesjs/src/services/metrics.prometheus.service.js
@@ -14,41 +14,5 @@ module.exports = {
     health(ctx) {
       return { name: this.name };
     }
-  },
-
-  startedZ() {
-    this.logger.info("starting prometheus service...");
-    this.client = require("prom-client");
-
-    if (this.settings.collectDefaultMetrics) {
-      this.timer = this.client.collectDefaultMetrics({
-        timeout: this.settings.timeout
-      });
-    }
-
-    try {
-      this.createMetrics(this.settings.metrics);
-    } catch (error) {
-      //when doing service hot reloading this can fail
-      this.logger.warn(
-        "failed createMetrics .. is ok if this is due to hot reloading - error:",
-        error
-      );
-    }
-
-    this.server.get("/metrics", (req, res) => {
-      res.setHeader("Content-Type", this.client.contentType);
-      res.end(this.client.register.metrics());
-    });
-
-    return this.server.listen(this.settings.port).then(() => {
-      this.logger.info(
-        `Prometheus collector is listening on port ${
-          this.settings.port
-        }, metrics exposed on /metrics endpoint`
-      );
-
-      this.updateCommonValues();
-    });
   }
 };
